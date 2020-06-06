409c6628ac915a3dfae8360b4da0d2e5cedbd7da
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-06-05 20:57:23 -0400

    init push

---
+++ ConsoleUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMGLib
+{
+    class ConsoleUtils
+    {
+        //Error text in console! Changes text to red.
+        public static void consoleError(string appID, string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[" + appID + "] " + error);
+        }
+
+        //Info text in console! Changes text to blue.
+        public static void consoleInfo(string appID, string info)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("[" + appID + "] " + info);
+        }
+
+        //Success text in console! Changes text to green.
+        public static void consoleSuccess(string appID, string success)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[" + appID + "] " + success);
+        }
+
+        //Warning text in console! Changes text to yellow.
+        public static void consoleWarning(string appID, string warning)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[" + appID + "] " + warning);
+        }
+
+        //Default text in console. Color is white.
+        public static void consoleOutput(string appID, string output)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("[" + appID + "] " + output);
+        }
+    }
+}
---
+++ RandomUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace TMGLib
+{
+    public class RandomUtils
+    {
+        /*
+         * Function to randomly generate numbers input by user.
+         * int min = Minimum number to be chosen
+         * int max = Maximum number to be chosen
+         * 
+         * returns int
+         */
+        public static int getRNG(int min, int max)
+        {
+            if (max <= min)
+                max = min + 1;
+            return new Random().Next(min, max);
+        }
+        /*
+         * Function to randomly generate numbers input by user.
+         * Uses int min and int max as values.
+         * 
+         * returns int
+         */
+        public static int getRNG()
+        {
+            return new Random().Next(int.MinValue, int.MaxValue);
+        }
+    }
+}
